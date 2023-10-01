721b81dab1e7081740fb60f6a566c3b133fb7f3b
Tom Gammeur <101002853+tomgammeur@users.noreply.github.com>
2023-10-01 12:41:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function() {
+    // Une fois que tout le contenu est chargé, masquez l'écran de chargement.
+    const loaderWrapper = document.querySelector(".loader-wrapper");
+    loaderWrapper.style.display = "none";
+});
